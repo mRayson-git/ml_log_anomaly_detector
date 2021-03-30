0efc5831f3691316ac037d0781b9567bd2269915
Michael Rayson <58303842+mRayson-git@users.noreply.github.com>
2021-03-30 08:54:06 -0400

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -6,6 +6,7 @@ if fname.endswith("tar.gz"):
     tar = tarfile.open(fname, "r:gz")
     tar.extractall()
     tar.close()
+    print("File has been extracted")
 elif fname.endswith("tar"):
     tar = tarfile.open(fname, "r:")
     tar.extractall()
