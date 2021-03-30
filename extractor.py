49c7ceb286f6597fa91d62df4c735f12b28278de
Michael Rayson <58303842+mRayson-git@users.noreply.github.com>
2021-03-30 08:08:44 -0400

    Create extractor.py

---
+++ extractor.py
@@ -0,0 +1,12 @@
+import tarfile
+
+fname = "Thunderbird.tar.gz"
+
+if fname.endswith("tar.gz"):
+    tar = tarfile.open(fname, "r:gz")
+    tar.extractall()
+    tar.close()
+elif fname.endswith("tar"):
+    tar = tarfile.open(fname, "r:")
+    tar.extractall()
+    tar.close()
