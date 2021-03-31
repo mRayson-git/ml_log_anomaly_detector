abe7e6ed8bdae1ba8ab438f6355af9c7c5140bc7
Michael Rayson <58303842+mRayson-git@users.noreply.github.com>
2021-03-31 13:01:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 import psycopg2
 
-conn = psycopg2.connect(
-  host = "172.30.136.222",
-  database = "testdb",
-  user = "postgres",
-  password = "postgres")
-print(conn)
+try:
+  conn = psycopg2.connect(
+    host = "172.30.136.222",
+    database = "testdb",
+    user = "postgres",
+    password = "postgres")
+
+  # Create a cursor
+  cursor = conn.cursor()
+
+  # execute a statement
+  print('PostgreSQL database version:')
+  cur.execute('SELECT version()')
+  
+  # display the PostgreSQL database server version
+  db_version = cur.fetchone()
+  print(db_version)
+
+except (Exception, psycopg2.DatabaseError) as error:
+  print(error)
+
+finally:
+  if conn is not None:
+    conn.close()
+    print('Database connection closed.')
