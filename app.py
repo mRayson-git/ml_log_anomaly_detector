9c3dc1e7cf33ab2b1fe315fd5babfb2f38c9d0f2
Michael Rayson <58303842+mRayson-git@users.noreply.github.com>
2021-03-31 09:55:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname=suppliers user=postgres password=postgres")
+conn = psycopg2.connect(
+  host = 172.30.136.222,
+  database = "suppliers",
+  user = "postgres",
+  password = "postgres")
 print(conn)
